e392fea7fb9683fa156fae37967e5856ad8e72a2
Cyua <466764008@qq.com>
2018-08-29 15:21:07 +0800

    add threaded

--- project/run.py
+++ project/run.py
@@ -4,4 +4,4 @@ from backend import app
 from backend import config
 
 if __name__ == "__main__":
-    app.run(port=config.WEB_SERVER_PORT, host="0.0.0.0", debug=True)
+    app.run(port=config.WEB_SERVER_PORT, host="0.0.0.0", debug=True, threaded=True)
